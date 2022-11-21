531b2b66f3f56e857b2c9d3c96ba08a763df12a0
Eze250891 <102075846+Eze250891@users.noreply.github.com>
2022-11-21 20:30:57 -0300

    css + java funciones

---
+++ index.js
@@ -0,0 +1,9 @@
+const displayValorAnterior = document.getElementById('valor-anterior');
+const displayValorActual = document.getElementById('valor-actual');
+
+const buttonsNums = document.querySelectorAll('.numero');
+const buttonsOper = document.querySelectorAll('.operador');
+
+const calculadora = new Calculadora();
+console.log(calculadora.dividir (2,3))
+
